c7ddd0533a774a4149206916287b488113338ddb
Pedro Silva <pedrosilva354@gmail.com>
2026-05-25 19:18:07 -0300

    chore: finalizando backend setup novamente

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,14 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+          "@typescript-eslint/no-require-imports": "off"
+        },
+    },
+    {
+        ignores: ["dist/**", "node_modules/**"]
+    }
+    ]
